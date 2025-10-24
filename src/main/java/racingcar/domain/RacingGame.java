019bf84d5135922222765005a03cfcd7a9c1f3d5
xodbs126 <dltvmsskd@naver.com>
2025-10-25 01:27:20 +0900

    feat : 레이싱 게임을 실행하는 로직을 담당하는 RacingGame 클래스 구현

---
+++ src/main/java/racingcar/domain/RacingGame.java
@@ -0,0 +1,68 @@
+package racingcar.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+import racingcar.dto.RacingGameDto;
+import racingcar.view.OutputView;
+
+public class RacingGame {
+
+    private final ParticipantGenerater participantGenerater;
+    private final RandomNumberGenerator randomNumberGenerator;
+    private final OutputView outputView;
+
+    public RacingGame(ParticipantGenerater participantGenerater, RandomNumberGenerator randomNumberGenerator,
+                      OutputView outputView) {
+        this.participantGenerater = participantGenerater;
+        this.randomNumberGenerator = randomNumberGenerator;
+        this.outputView = outputView;
+    }
+
+    public List<Participant> play(RacingGameDto racingGameDto) {
+        List<Participant> participants = generateParticipants(racingGameDto);
+
+        int count = racingGameDto.count();
+        for (int i = 0; i < count; i++) {
+            gameProcess(participants);
+            outputView.printProcess(participants);
+        }
+
+        List<Participant> winnerList = getWinners(participants);
+
+        return winnerList;
+
+    }
+
+    private List<Participant> getWinners(List<Participant> participants) {
+
+        int maxPosition = 0;
+        for (Participant participant : participants) {
+            maxPosition = Math.max(maxPosition, participant.getCarPosition());
+        }
+
+        List<Participant> winners = new ArrayList<>();
+        for (Participant participant : participants) {
+            if (participant.getCarPosition() == maxPosition) {
+                winners.add(participant);
+            }
+        }
+
+        return winners;
+    }
+
+    private void gameProcess(List<Participant> participants) {
+
+        for (Participant participant : participants) {
+            Integer randomNumber = randomNumberGenerator.generateRandomNumber();
+            participant.move(randomNumber);
+        }
+
+    }
+
+    private List<Participant> generateParticipants(RacingGameDto racingGameDto) {
+        String participantList = racingGameDto.names();
+        List<Participant> participants = participantGenerater.generate(participantList);
+
+        return participants;
+    }
+}
