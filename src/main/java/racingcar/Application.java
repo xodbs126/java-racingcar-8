c016b51aa4b6befc95ae035e86a126d8aebe10e8
xodbs126 <dltvmsskd@naver.com>
2025-10-25 01:29:06 +0900

    feat : 애플리케이션 의존성 주입

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,24 @@
 package racingcar;
 
+import racingcar.controller.RacingGameController;
+import racingcar.domain.ParticipantGenerater;
+import racingcar.domain.RacingGame;
+import racingcar.domain.RandomNumberGenerator;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        InputView inputView = new InputView();
+        OutputView outputView = new OutputView();
+
+        ParticipantGenerater participantGenerater = new ParticipantGenerater();
+        RandomNumberGenerator randomNumberGenerator = new RandomNumberGenerator();
+
+        RacingGame racingGame = new RacingGame(participantGenerater, randomNumberGenerator, outputView);
+        RacingGameController racingGameController = new RacingGameController(inputView, outputView, racingGame);
+
+        racingGameController.play();
+
     }
 }
