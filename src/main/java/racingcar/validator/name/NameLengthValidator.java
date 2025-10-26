305c4a149511adb97de8e26c2fbe0777ba44f02e
xodbs126 <dltvmsskd@naver.com>
2025-10-26 23:39:42 +0900

    refactor : 이름 길이 제한 상수 정의

--- src/main/java/racingcar/validator/name/NameLengthValidator.java
+++ src/main/java/racingcar/validator/name/NameLengthValidator.java
@@ -2,9 +2,10 @@ package racingcar.validator.name;
 
 public class NameLengthValidator implements NameValidator {
 
+    private static Integer NAME_LENGTH = 5;
     @Override
     public void isValid(String name) {
-        if (name.length() > 5) {
+        if (name.length() > NAME_LENGTH) {
             throw new IllegalArgumentException("이름은 5자 이하로만 가능합니다.");
         }
     }
