0ef51842bb7fee93feec653372d988a07f4d3e75
xodbs126 <dltvmsskd@naver.com>
2025-10-26 11:21:02 +0900

    feat : InputView 각 Validator 로직 추가

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -3,24 +3,46 @@ package racingcar.view;
 
 import camp.nextstep.edu.missionutils.Console;
 import racingcar.dto.RacingGameDto;
+import racingcar.validator.input.CountValidator;
+import racingcar.validator.input.NameValidator;
 
 public class InputView {
 
-    private static int inputCount() {
-        System.out.println("시도할 횟수는 몇 회인가요?");
-        int count = Integer.parseInt(Console.readLine());
-        return count;
+    private final CountValidator countValidator;
+    private final NameValidator nameValidator;
+
+    public InputView(CountValidator countValidator, NameValidator nameValidator) {
+        this.countValidator = countValidator;
+        this.nameValidator = nameValidator;
     }
 
-    private static String inputName() {
+    private int inputCount() {
+        System.out.println("시도할 횟수는 몇 회인가요?");
+        try {
+            int count = Integer.parseInt(Console.readLine());
+            countValidator.isValid(count);
+
+            return count;
+
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("유효한 시도 횟수의 입력이 아닙니다.");
+        }
+    }
+
+
+    private String inputName() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+
         String names = Console.readLine();
+
+        nameValidator.isValid(names);
         return names;
     }
 
     public RacingGameDto input() {
         String names = inputName();
         int count = inputCount();
+        ;
 
         return RacingGameDto.from(names, count);
     }
