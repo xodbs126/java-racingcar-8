998d4dc7fd3e2bdff41021e3a97ddf600f109aeb
xodbs126 <dltvmsskd@naver.com>
2025-10-26 10:52:24 +0900

    test : InputView 이름 미입력 테스트 코드

--- src/test/java/racingcar/view/InputViewTest.java
+++ src/test/java/racingcar/view/InputViewTest.java
@@ -10,10 +10,15 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import racingcar.dto.RacingGameDto;
+import racingcar.validator.input.CountRangeValidator;
+import racingcar.validator.input.NullNameValidator;
 
 class InputViewTest {
 
-    private final InputView inputView = new InputView();
+    private final CountRangeValidator countRangeValidator = new CountRangeValidator();
+    private final NullNameValidator nameValidator = new NullNameValidator();
+
+    private final InputView inputView = new InputView(countRangeValidator,nameValidator);
     private final InputStream originalIn = System.in;
 
     @AfterEach
@@ -49,6 +54,20 @@ class InputViewTest {
         // given
         String names = "pobi,woni";
         String invalidCount = "abc";
+        String input = names + "\n" + invalidCount;
+        simulateInput(input);
+
+        // when / then
+        assertThatThrownBy(inputView::input)
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("이름을 미입력하면 IllegalArgumentException이 발생")
+    void 이름_미입력() {
+        // given
+        String names = "";
+        String invalidCount = "5";
         String input = names + "\n" + invalidCount;
         simulateInput(input);
 
