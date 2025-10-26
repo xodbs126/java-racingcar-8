507add2c01da19398a67a696cb664d01f2eb4786
xodbs126 <dltvmsskd@naver.com>
2025-10-26 16:55:35 +0900

    refactor : 애플리케이션에 추가 의존성 주입

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,25 +1,37 @@
 package racingcar;
 
+import java.util.List;
 import racingcar.controller.RacingGameController;
 import racingcar.domain.ParticipantGenerater;
 import racingcar.domain.RacingGame;
 import racingcar.domain.RandomNumberGenerator;
-import racingcar.validator.input.CountRangeValidator;
-import racingcar.validator.input.NullNameValidator;
+import racingcar.role.GameRole;
+import racingcar.role.RandomNumberRole;
+import racingcar.validator.count.CountRangeValidator;
+import racingcar.validator.name.NameLengthValidator;
+import racingcar.validator.name.NameValidator;
+import racingcar.validator.name.NullNameValidator;
 import racingcar.view.InputView;
 import racingcar.view.OutputView;
 
 public class Application {
     public static void main(String[] args) {
         CountRangeValidator countRangeValidator = new CountRangeValidator();
-        NullNameValidator nameValidator = new NullNameValidator();
-        InputView inputView = new InputView(countRangeValidator,nameValidator);
+        NullNameValidator nullNameValidator = new NullNameValidator();
+        NameLengthValidator nameLengthValidator = new NameLengthValidator();
+
+        InputView inputView = new InputView(countRangeValidator,nullNameValidator);
         OutputView outputView = new OutputView();
 
-        ParticipantGenerater participantGenerater = new ParticipantGenerater();
+        List<NameValidator> nameValidators = List.of(nullNameValidator, nameLengthValidator);
+
+        ParticipantGenerater participantGenerater = new ParticipantGenerater(nameValidators);
         RandomNumberGenerator randomNumberGenerator = new RandomNumberGenerator();
 
-        RacingGame racingGame = new RacingGame(participantGenerater, randomNumberGenerator, outputView);
+        RandomNumberRole randomNumberRole = new RandomNumberRole();
+        List<GameRole> gameRoles = List.of(randomNumberRole);
+
+        RacingGame racingGame = new RacingGame(participantGenerater, randomNumberGenerator, outputView,gameRoles);
         RacingGameController racingGameController = new RacingGameController(inputView, outputView, racingGame);
 
         racingGameController.play();
